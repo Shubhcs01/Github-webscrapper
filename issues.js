74ef87c0af9fa65908adf48861bbe2329ede7d32
Shubham Gupta <shubhcs01@gmail.com>
2022-01-02 11:30:34 +0530

    updated

--- issues.js
+++ issues.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const path = require('path');
 const pdfkit = require('pdfkit');
 
-function getIssuesPageHtml(url,topicName, repoName) {
+function getIssuesPageHtml(url, topicName, repoName) {
     request(url, cb);
     function cb(err, response, html) {
         if (err) {
             console.log(err);
-        } else if(response.statusCode == 404){
-            console,log("Page not found");
+        } else if (response.statusCode == 404) {
+            console.log("Page not found");
         } else {
             getIssues(html);
         }
@@ -21,19 +21,19 @@ function getIssuesPageHtml(url,topicName, repoName) {
         let issueArr = [];
         for (let i = 0; i < issues.length; i++) {
             let issueLink = selTool(issues[i]).attr("href");
-            if(issueLink != undefined){
-                issueArr.push(issueLink);
+            if (issueLink != undefined) {
+                issueArr.push("https://github.com" + issueLink);
             }
         }
         console.log(topicName + "/" + repoName);
         console.log("```````````````````````````");
-        console.log(issueArr)
+        console.log(issueArr);
         console.log("```````````````````````````");
 
-        let folderPath = path.join(__dirname,topicName);
+        let folderPath = path.join(__dirname, topicName);
         dirCreate(folderPath);
 
-        let filePath = path.join(folderPath, repoName+".pdf");
+        let filePath = path.join(folderPath, repoName + ".pdf");
         // fs.writeFileSync(filePath, JSON.stringify(issueArr));  //--> write on a JSON file after converting Array to JSON String. We can't write Array directly.
         let text = JSON.stringify(issueArr);
 
@@ -43,12 +43,12 @@ function getIssuesPageHtml(url,topicName, repoName) {
         pdfDoc.fillColor('blue').list(issueArr);
         pdfDoc.moveDown(0.5);  //--> Move down a bit to provide space between lists
         pdfDoc.end();
-         
+
     }
 }
 
-function dirCreate(filepath){
-    if(fs.existsSync(filepath)==false){
+function dirCreate(filepath) {
+    if (fs.existsSync(filepath) == false) {
         fs.mkdirSync(filepath);
     }
 }
